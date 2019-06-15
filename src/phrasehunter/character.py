4dcbec7a71c680fed02f997b0933789a3646862d
Henrik Christensen <sensen1695@hotmail.com>
2019-06-15 19:13:47 +0200

    added Character class

--- src/phrasehunter/character.py
+++ src/phrasehunter/character.py
@@ -1,1 +1,25 @@
-# Create your Character class logic in here.
+class Character:
+    """Responsible for holding the state of a given character"""
+
+    def __init__(self, char: str) -> None:
+        self.check_if_valid_char(char)
+        self.char = char
+        self.was_guessed = False
+
+    def check_if_valid_char(self, char: str) -> None:
+        """Checks if char is a character"""
+        if len(char) != 1:
+            raise ValueError('Invalid input: Please enter a single character')
+        elif not char.isalpha():
+            raise ValueError('Invalid input: Please enter a character')
+
+    def guess(self, char: str) -> None:
+        """Checks if char is equal to self.char"""
+        self.check_if_valid_char(char)
+
+        if self.char == char:
+            self.was_guessed = True
+
+    def display_char(self) -> None:
+        """Displays self.char if has been guessed, _ otherwise"""
+        print(self.char if self.was_guessed else '_')
