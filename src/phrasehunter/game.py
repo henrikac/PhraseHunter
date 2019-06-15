dacad1686bcbb62ff3826211aacf93e5b8cfe5f0
Henrik Christensen <sensen1695@hotmail.com>
2019-06-15 20:15:53 +0200

    added Game class

--- src/phrasehunter/game.py
+++ src/phrasehunter/game.py
@@ -1,1 +1,21 @@
-# Create your Game class logic in here.
+from random import randint
+from typing import List
+
+from phrase import Phrase
+
+
+class Game:
+    """Responsible for the state of the Game"""
+
+    def __init__(self, phrases: List[Phrase]) -> None:
+        if len(phrases) < 1:
+            raise ValueError('At least 1 phrase is required')
+
+        self.phrases = phrases
+        self.active_phrase = self.__get_random_phrase()
+
+    def __get_random_phrase(self) -> Phrase:
+        """Gets a random phrase"""
+        idx = randint(0, len(self.phrases) - 1)
+        return self.phrases[idx]
+
