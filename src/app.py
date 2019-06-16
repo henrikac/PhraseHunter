f755650c97818fc0bc8ac7fc509a14f7ed42f343
Henrik Christensen <sensen1695@hotmail.com>
2019-06-16 22:14:34 +0200

    basic game added

--- src/app.py
+++ src/app.py
@@ -1,6 +1,32 @@
-# Import your Game class
+from typing import List
 
-# Create your Dunder Main statement.
+from phrasehunter.character import Character
+from phrasehunter.game import Game
+from phrasehunter.phrase import Phrase
+
+
+def create_phrases() -> List[Phrase]:
+    """Creates a list of phrases"""
+    phrases: List[Phrase] = []
+    str_phrases = [
+        'hello',
+        'cowboy',
+        'christmas',
+        'tomato',
+        'univers'
+    ]
+
+    for phrase in str_phrases:
+        chars = [Character(char) for char in phrase]
+        phrases.append(Phrase(chars))
+
+    return phrases
+
+
+if __name__ == '__main__':
+    phrases = create_phrases()
+    game = Game(phrases)
+    game.play()
 
 # Inside Dunder Main:
 ## Create an instance of your Game class
--- src/phrasehunter/game.py
+++ src/phrasehunter/game.py
@@ -1,7 +1,8 @@
+import os
 from random import randint
 from typing import List
 
-from phrase import Phrase
+from .phrase import Phrase
 
 
 class Game:
@@ -13,9 +14,40 @@ class Game:
 
         self.phrases = phrases
         self.active_phrase = self.__get_random_phrase()
+        self.remaining_tries = 5
 
     def __get_random_phrase(self) -> Phrase:
         """Gets a random phrase"""
         idx = randint(0, len(self.phrases) - 1)
         return self.phrases[idx]
+
+    def __clear_screen(self) -> None:
+        """Clears the screen"""
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    def __prompt_player(self) -> str:
+        """Prompts the player for a character"""
+        while True:
+            guess = input('Enter a character: ')
+
+            if len(guess) != 1 or not guess.isalpha():
+                print('Please enter a single character')
+            else:
+                return guess
+
+    def play(self) -> None:
+        """Starts the game"""
+        while True:
+            self.__clear_screen()
+            print('=' * 12)
+            print('PHRASEHUNTER')
+            print('=' * 12)
+
+            print(f'\n{self.active_phrase.display_phrase()}\n')
+            print(f'You have {self.remaining_tries} {"life" if self.remaining_tries < 2 else "lives"} left\n')
+
+            user_input = self.__prompt_player()
+
+            for char in self.active_phrase:
+                char.guess(user_input)
 
